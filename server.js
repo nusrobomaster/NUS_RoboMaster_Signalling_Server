7c4dce87b1a1d4751556a0b482b138188db590af
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-08-10 02:21:59 +0800

    Remove separate candidate functionality

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-//require our websocket library 
 var WebSocketServer = require('ws').Server;
- 
-//creating a websocket server at port 9090 
 var wss = new WebSocketServer({port: 49621}); 
 
-//all connected to the server users 
-var users = {};
+// Stores all users connected to server, including robots
+var connectedUsers = {};
   
 //when a user connects to our sever 
 wss.on('connection', function(connection) {
@@ -26,58 +23,54 @@ wss.on('connection', function(connection) {
 		
       //switching type of the user message 
       switch (data.type) { 
-         //when a user tries to login 
 			
-         case "login": 
-            console.log("User logged", data.name); 
-				
-            //if anyone is logged in with this username then refuse 
-            if(users[data.name]) { 
+         case "login": 				
+            if (connectedUsers[data.name]) { 
+               console.log("User attempted login with duplicate name: " + data.name);
                sendTo(connection, { 
                   type: "login", 
                   success: false 
-               }); 
+               });
             } else { 
                //save user connection on the server 
-               users[data.name] = connection; 
+               connectedUsers[data.name] = connection; 
                connection.name = data.name; 
-					
+               
+               console.log("User successfully logged in: " + data.name);
+               
                sendTo(connection, { 
                   type: "login", 
                   success: true 
                }); 
             } 
-				
             break; 
-				
+            
+            
          case "offer": 
-            //for ex. UserA wants to call UserB 
             console.log("Sending offer to: ", data.name); 
-				
-            //if UserB exists then send him offer details 
-            var conn = users[data.name];
-				
-            if(conn != null) { 
-               //setting that UserA connected with UserB 
+
+            var targetConnection = connectedUsers[data.name];
+            if (targetConnection != null) { 
                connection.otherName = data.name; 
 					
-               sendTo(conn, { 
+               sendTo(targetConnection, { 
                   type: "offer", 
                   offer: data.offer, 
                   name: connection.name 
                }); 
             } 
-				
+
             break;  
-				
+            
+            
          case "answer": 
             console.log("Sending answer to: ", data.name); 
-            //for ex. UserB answers UserA 
-            var conn = users[data.name]; 
+
+            var targetConnection = connectedUsers[data.name]; 
 				
-            if(conn != null) { 
+            if(targetConnection != null) { 
                connection.otherName = data.name; 
-               sendTo(conn, { 
+               sendTo(targetConnection, { 
                   type: "answer", 
                   answer: data.answer 
                }); 
@@ -85,22 +78,9 @@ wss.on('connection', function(connection) {
 				
             break;  
 				
-         case "candidate": 
-            console.log("Sending candidate to:",data.name); 
-            var conn = users[data.name];  
-				
-            if(conn != null) { 
-               sendTo(conn, { 
-                  type: "candidate", 
-                  candidate: data.candidate 
-               });
-            } 
-				
-            break;  
-				
          case "leave": 
             console.log("Disconnecting from", data.name); 
-            var conn = users[data.name]; 
+            var conn = connectedUsers[data.name]; 
             conn.otherName = null; 
 				
             //notify the other user so he can disconnect his peer connection 
@@ -129,11 +109,11 @@ wss.on('connection', function(connection) {
         console.log("User leaving");
 
       if(connection.name) { 
-      delete users[connection.name]; 
+      delete connectedUsers[connection.name]; 
 		
          if(connection.otherName) { 
             console.log("Disconnecting from ", connection.otherName);
-            var conn = users[connection.otherName]; 
+            var conn = connectedUsers[connection.otherName]; 
             conn.otherName = null;  
 				
             if(conn != null) { 
